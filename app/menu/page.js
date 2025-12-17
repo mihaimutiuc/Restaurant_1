5c0f60121f33a2260d7ff7455467cc07f88f9de3
Mihai Mutiuc <mihaimutiuc@example.com>
2025-12-17 14:25:13 +0000

    Remove emoji icons from categories, show only images

--- app/menu/page.js
+++ app/menu/page.js
@@ -183,7 +183,7 @@ export default function MenuPage() {
                         : "text-gray-600 hover:bg-gray-50 hover:text-orange-600"
                     }`}
                   >
-                    {category.image ? (
+                    {category.image && (
                       <div className="relative w-6 h-6 rounded-full overflow-hidden">
                         <Image
                           src={category.image}
@@ -192,8 +192,6 @@ export default function MenuPage() {
                           className="object-cover"
                         />
                       </div>
-                    ) : (
-                      <span className="text-lg">{category.icon}</span>
                     )}
                     <span>{category.name}</span>
                     {activeCategory === category.id && (
